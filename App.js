030fa8b6a5a39b5984d9f81464c2b827d4f5bcb2
Ritesh Rana <rana1997ritesh@gmail.com>
2022-11-29 08:57:43 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,146 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useRef } from "react";
+import {
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+} from "react-native";
+import { styled, TailwindProvider } from "tailwindcss-react-native";
+import LinearGradient from "react-native-linear-gradient";
+// import AnimetedBtn from "./AnimetedBtn";
 export default function App() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  // const fadeAnimText = useRef("Join")).current;
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    // Animated.timing(fadeAnimText, {
+    //   toValue: "✓",
+    //   duration: 2000,
+    // }).start();
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+    }).start();
+    setTimeout(() => {
+      fadeOut();
+    }, 2000);
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 3 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 3000,
+    }).start();
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <View style={styles.box}>
+            <Text className="" style={styles.textColor}>
+              {"Press prototype to see "}
+            </Text>
+            <Text className="" style={styles.textColor}>
+              {"button animation"}
+            </Text>
+            <Animated.View
+              style={[
+                styles.fadingContainer,
+                {
+                  // Bind opacity to animated value
+                  opacity: fadeAnim,
+                },
+              ]}
+            >
+              <Text style={styles.fadingText}>Fading View!</Text>
+            </Animated.View>
+            <View style={styles.buttonRow}>
+              {/* <Button
+                title="Fade In View"
+                style={[
+                  styles.btn,
+                  {
+                    // Bind opacity to animated value
+                    opacity: fadeAnim,
+                  },
+                ]}
+                textColor="#000"
+                color="transparent"
+                onPress={fadeIn}
+              > */}
+                <LinearGradient
+                  colors={["#ff00e0", "#c000ff", "#9100ff"]}
+                  style={styles.linearGradient}
+                  locations ={[0, 0.3, 0.8]}
+                  start={{ x: 0, y: 1 }}
+                  end={{ x: 1, y: 0 }}
+                  onPress={fadeIn}
+                >
+                  <Text onPress={fadeIn} style={styles.btnTextColor}>{"Join"}</Text>
+                </LinearGradient>
+              {/* </Button> */}
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </TailwindProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  textColor: {
+    color: "#000",
+    fontWeight: `bold`,
+  },
+  btnTextColor: {
+    color: "#fff",
+    fontWeight: `bold`,
+  },
+  btn: {
+    color: "#000",
+    backgroundColor: "red",
+    backgroundImage: "linear-gradient(to #ff00e0, #c000ff, #9100ff ) ",
+  },
+  box: {
+    backgroundColor: "#fff",
+    color: "#000",
+    padding: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    color: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fadingContainer: {
+    padding: 20,
+    backgroundColor: "powderblue",
+    backgroundImage: "linear-gradient(to #ff00e0, #c000ff, #9100ff ) ",
+  },
+  fadingText: {
+    fontSize: 28,
+  },
+  buttonRow: {
+    color: "#000",
+    backgroundImage: "linear-gradient(from #ff00e0,to #9100ff )",
+  },
+  linearGradient: {
+    color:"#fff",
+    margin: 20,
+    paddingVertical:5,
+    paddingHorizontal:20,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    height: "auto",
+    width: "auto",
+    fontWeight: "bold"
+    
   },
 });
